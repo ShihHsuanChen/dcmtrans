349afbe4ace65955ca231d5120cf40838b8662aa
j0018 <j0018@muenai.com>
2020-11-11 17:48:14 +0800

    fix ? read from buffer

--- dcmtrans/dcm_imread.py
+++ dcmtrans/dcm_imread.py
@@ -1,3 +1,4 @@
+import PIL
 import logging
 import pydicom
 import numpy as np
@@ -5,11 +6,11 @@ import SimpleITK as sitk
 from typing import Union
 
 
-__all__ = ['read_pixel', 'read_pixel_pil', 'read_pixel_pydicom', 'read_pixel_sitk']
+__all__ = ['read_pixel', 'read_pixel_bytes', 'read_pixel_pydicom', 'read_pixel_sitk']
 
 
 def read_pixel(filename: str, return_on_fail=None):
-    for method in [read_pixel_pydicom, read_pixel_sitk, read_pixel_pil]:
+    for method in [read_pixel_pydicom, read_pixel_sitk, read_pixel_bytes]:
         try:
             img_arr = method(filename)
             method_name = method.__qualname__
@@ -38,7 +39,7 @@ def read_pixel_sitk(filename: str):
     return sitk.GetArrayFromImage(itkimg)
 
 
-def read_pixel_pil(dcm: Union[str, pydicom.dataset.FileDataset]):
+def read_pixel_bytes(dcm: Union[str, pydicom.dataset.FileDataset]):
     if isinstance(dcm, str):
         data = pydicom.dcmread(dcm, force=True)
     elif isinstance(dcm, pydicom.dataset.FileDataset):
@@ -48,23 +49,36 @@ def read_pixel_pil(dcm: Union[str, pydicom.dataset.FileDataset]):
     rows = int(data.Rows)
     cols = int(data.Columns)
     spx = int(data.SamplesPerPixel)
-    nbits = int(len(data.PixelData) * 8 / rows / cols / spx)
+    nbits = len(data.PixelData) * 8 / rows / cols / spx
     slope = data.get('RescaleSlope', None)
     unit = data.get('RescaleType', None)
     modality = data.get('Modality', None)
     modality = str(modality).strip() if modality is not None else None
     signed = slope is not None and int(slope) == 1 and (unit is not None or modality in ['CT', 'MR'])
 
-    if nbits == 8 and not signed:
-        dtype = np.uint8
-    elif nbits == 8 and signed:
-        dtype = np.int8
-    elif nbits == 16 and not signed:
-        dtype = np.uint16
-    elif nbits == 16 and signed:
-        dtype = np.int16
+    if not nbits.is_integer():
+        if spx == 1:
+            fmt = 'L'
+        elif spx == 3:
+            fmt = 'RGB'
+        elif spx == 4:
+            fmt = 'CMYK'
+        else:
+            raise ValueError('SamplesPerPixel is larger than 4')
+        img = PIL.Image.frombuffer(fmt, (rows, cols), data.PixelData, 'raw')
+        img_arr = np.array(img)
     else:
-        dtype = getattr(np, f'uint{nbits}')
-    img_arr = np.frombuffer(data.PixelData, dtype)
-    img_arr = img_arr.reshape([rows, cols])
+        nbits = int(nbits)
+        if nbits == 8 and not signed:
+            dtype = np.uint8
+        elif nbits == 8 and signed:
+            dtype = np.int8
+        elif nbits == 16 and not signed:
+            dtype = np.uint16
+        elif nbits == 16 and signed:
+            dtype = np.int16
+        else:
+            dtype = getattr(np, f'uint{nbits}')
+        img_arr = np.frombuffer(data.PixelData, dtype)
+        img_arr = img_arr.reshape([rows, cols])
     return img_arr
