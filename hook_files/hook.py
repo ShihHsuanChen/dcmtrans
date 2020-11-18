821e145dc0087e15381ebf83601c1c3322bacaa8
j0018 <j0018@muenai.com>
2020-11-18 11:44:54 +0800

    add hook for pylibjpeg-libjpeg

--- hook_files/hook.py
+++ hook_files/hook.py
@@ -1,5 +1,11 @@
+from PyInstaller.utils.hooks import copy_metadata
+
+
 hiddenimports = [
     'dcmtrans',
     'dcmtrans.reconstruction',
     'dcmtrans.trans_method',
 ]
+
+datas = copy_metadata('pylibjpeg')
+datas += copy_metadata('pylibjpeg_libjpeg')
