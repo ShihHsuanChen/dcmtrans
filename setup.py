9312dcd0c40b405a9fe38c08bb0e8f6b894fd3dc
ShihHsuanChen <j0018@muenai.com>
2021-09-13 13:48:27 +0800

    Add setup.py. Loose requirements version constrain

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+
+def get_requirements(fns):
+    reqs = []
+    for fn in fns:
+        if not os.path.exists(fn):
+            raise FileNotFoundError(f'Given file {fn} does not exists.')
+        with open(fn, 'r') as f:
+            reqs += [line.strip() for line in f.readlines()]
+    return reqs
+
+
+setup(
+    name='dcmtrans',
+    description='Library for reading various of dicom image(s).',
+    version_config={
+        'template': '{tag}',
+        'dev_template': '{tag}.post{ccount}',
+        'dirty_template': '{tag}.post{ccount}+dirty',
+    },
+    setup_requires=['setuptools-git-versioning'],
+    install_requires=get_requirements(['requirements.txt']),
+    packages=find_packages(exclude=['test']),
+)
+
